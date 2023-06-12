4811e053966f2003de39145447dc76279164b5f5
unknown <cursoweb600@hotmail.com>
2023-06-12 14:21:54 +0200

    Ejercicios Terminados

---
+++ Ejercicio3.js
@@ -0,0 +1,33 @@
+ //3.1
+ let tresPuntoUno = "Multiplica 10 por 5 y muestra el resultado mediante console.";
+ console.log (tresPuntoUno)
+ let multiplica = 5 * 10;
+ console.log (multiplica)
+//3.2
+let tresPuntoDos = "Divide 10 por 2 y muestra el resultado en un console.";
+console.log (tresPuntoDos)
+let divide = 10 / 2;
+console.log (divide)
+//3.3
+let tresPuntoTres = "Muestra mediante un console el resto de dividir 15 por 9.";
+console.log (tresPuntoTres)
+let resto = 15%9;
+console.log (resto)
+//3.4
+let tresPuntoCuatro = "Usa el correcto operador de asignación";
+console.log (tresPuntoCuatro)
+let operadorLogico = "suma";
+console.log (operadorLogico)
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log (o)
+//3.5
+let tresPuntoCinco = "Usa el correcto operador de asignación";
+console.log (tresPuntoCinco)
+let operadorLogico2 = "multiplicación";
+console.log (operadorLogico2)
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log (i)
